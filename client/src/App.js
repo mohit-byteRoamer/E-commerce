b5bb5bc3074d1488b7057865008f0f814614ab75
mohit-Byte-Roamer <mohit878421@gmail.com>
2023-10-05 08:49:40 +0530

    ready folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProductCard from "./Components/Widgets/product-card/index.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductCard />
+    </div>
+  );
+}
+
+export default App;
